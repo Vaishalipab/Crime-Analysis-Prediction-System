6b5e363bbea19d1682918060d77ecd5f027267af
Raghav Bansal <43716103+bansalraghav19@users.noreply.github.com>
2019-10-02 18:15:20 +0530

    Create new.cpp

---
+++ new.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+#define int int64_t
+int32_t main(){ 
+    int n;
+    std::cin>>n;
+    std::cout<<n<<'\n';
+    std::cout<<"Hello Hackoctoberfest!!"<<'\n';
+    return 0;
+}
